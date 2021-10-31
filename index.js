10bb281e4ecf8372e8576bb2c35714648d0c9829
Soto Alvarez del Castillo Gerardo Alberto <gerardo.8.soto@gmail.com>
2021-10-30 21:20:02 -0500

    Obtaining parameters using the GET method.

--- index.js
+++ index.js
@@ -10,26 +10,61 @@ app.get('/', (req, res) => {
   res.send('Hello, server on express');
 });
 
-
+// route home page
 app.get('/home', (req, res) => {
   res.send('Welcome to home!');
 });
 
 
+// route new product
 app.get('/new-path', (req, res) => {
   // Java Script Object Notation:
   res.json('Hello, I\'m a new path');
 });
 
-
-app.get('/products-json', (req, res) => {
-  res.json({
-    name: 'Product 1',
-    price: 120
-  })
+// route of user id
+app.get('/user/:id', (req, res) => {
+  const {id} = req.params;
+  res.json({userId: id});
 });
 
+// route of all products
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 120
+    },
+    {
+      name: 'Product 2',
+      price: 150
+    }
+  ]);
+});
 
+// route of a product
+app.get('/products/:id', (req, res) => {
+  // EMACScript 5-
+  //  const id = req.params.id;
+  // EMACScript 6+
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 150,
+  });
+});
+
+// route of detail product
+app.get('/products/:id/details', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    detail: 'Halloween.',
+  });
+});
+
+// route of all categories
 app.get('/categories', (req, res) => {
   res.send([{
     category: 'Comedy',
@@ -45,10 +80,28 @@ app.get('/categories', (req, res) => {
   }])
 });
 
+// route of some categories
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
 //run app:
 app.listen(port, () => {
   // console.log -> warning: Bad practice by Eslint:
   // console.log in Dev (yes) / console.log in Prod (NO)
   console.log('Server listen at http://localhost:' + port);
 });
+
+
+
+
+
+
+
+
+
 
