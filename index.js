f3eed6bbb76ebd06a5929001fe9a1680a39b5269
Soto Alvarez del Castillo Gerardo Alberto <gerardo.8.soto@gmail.com>
2021-11-11 12:59:42 -0600

    prepare to send production.

--- index.js
+++ index.js
@@ -10,16 +10,17 @@ const whitelist = ['http://127.0.0.1', 'http://github.gerardosoto.com/g-flix/'];
 const options = {
   origin: (origin, callback) => {
     //console.log(origin);
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin ) {
       callback(null, true);
     } else {
-      callback(new Error('Permission denied, you are not on the whitelist. Contact administrator.'));
+      callback(new Error('Permission denied, you ['+ origin +'] are not on the whitelist. Contact administrator.'));
     }
   }
 }
 
 app.use(cors(options));
-const port = 3000;
+//const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to receive JSON POST:
 app.use(express.json());
