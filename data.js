0e34783a1481c416aaf3345dff5cb35c518f0adb
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-11-18 14:00:03 -0600

    added data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+// info by videos
+let movies = [
+    {
+      name: 'NodeJS',
+      des: 'Lorem ipsum',
+      image: 'images/logo01.png'
+    },
+    {
+      name: 'PostgreSQL',
+      des: 'Lorem ipsum amet',
+      image: 'images/logo02.PNG'
+    },
+    {
+      name: 'Express.js',
+      des: 'Lorem ipsum dolor sit',
+      image: 'images/logo03.PNG'
+    },
+]
+  
