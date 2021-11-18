cc09ac0adbf070fcb9efab8a09c50dc90beff1c8
Gerardo Soto <58956725+Gerardo-Soto@users.noreply.github.com>
2021-11-18 14:25:57 -0600

    Added data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+// info by videos
+let movies = [
+    {
+      name: 'NodeJS',
+      des: 'Lorem ipsum',
+      image: 'images/logo01.png'
+    },
+    {
+      name: 'PostgreSQL',
+      des: 'Lorem ipsum amet',
+      image: 'images/logo02.PNG'
+    },
+    {
+      name: 'Express.js',
+      des: 'Lorem ipsum dolor sit',
+      image: 'images/logo03.PNG'
+    },
+    {
+      name: 'Express.js',
+      des: 'Lorem ipsum dolor sit',
+      image: 'images/logo03.PNG'
+    },
+]
